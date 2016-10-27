900dafc6aa617e20f9ab02ede64b9630f6b2948f
gzq <694326243@qq.com>
2016-10-27 21:59:40 +0800

    mybatis逆向工程，简单生成po和mapper

---
+++ src/main/java/Generator.java
@@ -0,0 +1,27 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by admin on 2016/10/27.
+ */
+
+public class Generator {
+
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("H:\\github\\mybatis-generator\\src\\main\\resources\\generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
